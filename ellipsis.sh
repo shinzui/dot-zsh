5bb87181f080dcfc11e497dfbdaedeef80b5e8d2
Nadeem Bitar <nadeem@gmail.com>
2016-01-05 11:19:01 -0800

    change shell to zsh

--- ellipsis.sh
+++ ellipsis.sh
@@ -1,13 +1,21 @@
-#!/bin/zsh
+#!/usr/bin/env bash
 #
 # shinzui/zsh ellipsis package
 
 # The following hooks can be defined to customize behavior of your package:
 pkg.install() {
   git.clone git@github.com:tarjoilija/zgen.git
-#     fs.link_files $PKG_PATH
+
+  change_shell
 }
 
+change_shell() {
+  if [ "$(command -v zsh)" != "$SHELL" ]; then
+    echo "Changing shell to zsh"
+    command -v zsh | sudo tee -a /etc/shells
+    sudo chsh -s "$(command -v zsh)" "${USER}"
+  fi
+}
 # pkg.push() {
 #     git.push
 # }
